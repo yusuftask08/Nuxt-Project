ddce892f2c6e5b5ff3bea174a158f5a3f3d249c5
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2022-03-03 13:58:32 +0300

    responsive added, request proxy, api key hidden, repository design added

--- components/Home.vue
+++ components/Home.vue
@@ -1,32 +1,71 @@
 <template>
-  <div
-    class="
-      relative
-      flex
-      items-top
-      justify-center
-      min-h-screen
-      bg-gray-100
-      sm:items-center sm:pt-0
-    "
-  >
-    <img class="object-cover h-50 w-100" :src="image" />
-    <button
+  <div class="mx-5 mt-2">
+    <nav class="border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+      <div
+        class="container flex flex-wrap justify-between items-center mx-auto"
+      >
+        <a class="flex items-center">
+          <span
+            class="
+              self-center
+              text-xl
+              font-semibold
+              whitespace-nowrap
+              dark:text-white
+            "
+            >Yengec App</span
+          >
+        </a>
+        <div class="flex md:order-2">
+          <button
+            type="button"
+            class="
+              bg-gray-300
+              hover:bg-gray-400
+              text-gray-800
+              font-bold
+              py-2
+              px-4
+              rounded
+              items-center
+            "
+            @click="Logout"
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+    </nav>
+    <div
       class="
-        bg-gray-300
-        hover:bg-gray-400
-        text-gray-800
-        font-bold
-        py-2
-        px-4
-        rounded
-        inline-flex
-        items-center
+        mt-16
+        relative
+        flex flex-col
+        items-top
+        justify-top
+        sm:items-center sm:pt-0
       "
-      @click="getImage()"
     >
-      <span>Change Images</span>
-    </button>
+      <button
+        class="
+          bg-gray-300
+          hover:bg-gray-400
+          text-gray-800
+          font-bold
+          py-2
+          mb-6
+          px-4
+          rounded
+          items-center
+        "
+        @click="getImage()"
+      >
+        <span>Random Images</span>
+      </button>
+      <div>
+        <img class="object-contain h-58 w-96" :src="image" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -36,7 +75,10 @@ export default {
   name: "Home",
   methods: {
     getImage() {
-      this.$store.dispatch("fetchImage");
+      this.$store.dispatch("photo/get");
+    },
+    Logout() {
+      this.$router.push("/login");
     },
   },
   created() {
@@ -44,7 +86,7 @@ export default {
   },
   computed: {
     ...mapGetters({
-      image: "getImageData",
+      image: "photo/getImageData",
     }),
   },
 };
